9fed147a16327854b8460a7922bd076659654191
Amonsuzuki <amonsuzuki@keio.jp>
2024-11-15 11:42:06 +0900

    new

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,20 @@ import express from "express";
 //.envファイルを読み込む
 dotenv.config();
 
+let postCount = 0;
+const app = express();
+app.listen(3000);
+app.post("/", function (req, res) {
+  console.log(`Received POST request.`);
+  postCount++;
+  if (postCount == 10) {
+    trigger();
+    postCount = 0;
+  }
+  res.send("POST response by glitch");
+});
+
+/*
 const app = express();
 
 const PORT: number = parseInt(process.env.PORT || "3000");
@@ -17,6 +31,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+*/
 
 //Botで使うGatewayIntents、partials
 const client = new Client({
